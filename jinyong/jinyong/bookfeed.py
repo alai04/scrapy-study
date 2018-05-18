391edae625f7591acf2b59165bbc68b5c6184892
alai04 <alai04@gmail.com>
2018-05-18 23:52:08 +0800

    refactor & beautify pdf

--- jinyong/jinyong/bookfeed.py
+++ jinyong/jinyong/bookfeed.py
@@ -22,17 +22,18 @@ class BookFeed:
                         print(line, file=f)
             return fn
 
+    def book2Markdown(self, name, f):
+        f.write('# <center>%s</center>\n' % name)
+        for chap in sorted(self.books[name]):
+            f.write('## <center>%s</center>\n' % chap[1])
+            for line in chap[2]:
+                f.write('%s\n\n' % line)
+
     def getBookMarkdown(self, name):
         if self.books[name]:
-            book = self.books[name]
-            book.sort()
             fn = '%s.md' % name
             with open(fn, 'w') as f:
-                for chap in book:
-                    print('# %s' % chap[1], file=f)
-                    for line in chap[2]:
-                        print(line, file=f)
-                        print(file=f)
+                self.book2Markdown(name, f)
             return fn
 
     def getBookHTML(self, name):
@@ -62,23 +63,19 @@ class BookFeed:
         fn = '%s.md' % BOT_NAME
         with open(fn, 'w') as f:
             for name in self.books.keys():
-                print('# %s' % name, file=f)
-                for chap in sorted(self.books[name]):
-                    print('## %s' % chap[1], file=f)
-                    for line in chap[2]:
-                        print(line, file=f)
-                        print(file=f)
+                self.book2Markdown(name, f)
         return fn
 
     def getAllBooks1HTML(self):
-        fn = self.getAllBooks1Markdown()
-        if fn:
-            fnOut = '%s.html' % BOT_NAME
-            with open(fnOut, 'w') as f:
-                f.write('<html>\n<head>\n<title>%s</title>\n</head>\n<body>\n' % BOT_NAME)
+        fnOut = '%s.html' % BOT_NAME
+        with open(fnOut, 'w') as f:
+            f.write('<html>\n<head>\n<title>%s</title>\n</head>\n<body>\n' % BOT_NAME)
+            for name in self.books.keys():
+                fn = self.getBookMarkdown(name)
                 f.write(markdown2.markdown_path(fn))
-                f.write('</body>\n</html>\n')
-            return fnOut
+                f.write('<p style="page-break-before: always"></p>\n')
+            f.write('</body>\n</html>\n')
+        return fnOut
 
     def getAllBooks1PDF(self):
         fn = self.getAllBooks1HTML()
